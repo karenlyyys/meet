0fee1a07234c791746b516b59c75a0c73bc4a208
karenlyyys <99241654+karenlyyys@users.noreply.github.com>
2022-07-29 20:36:21 -0700

    msg

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -117,20 +117,26 @@ module.exports.getAccessToken = async (event) => {
         redirect_uris[0]
       );
       // Decode authorization code extracted from the URL query
-      const code = decodeURIComponent(`${event.pathParameters.code}`);
-    
+      const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+      oAuth2Client.setCredentials({ access_token });
+
       return new Promise((resolve, reject) => {
-        /**
-         *  Exchange authorization code for access token with a “callback” after the exchange,
-         *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
-         */
-    
-        oAuth2Client.getToken(code, (err, token) => {
-          if (err) {
-            return reject(err);
+        calendar.events.list(
+          {
+            calendarId: calendar_id,
+            auth: oAuth2Client,
+            timeMin: new Date().toISOString(),
+            singleEvents: true,
+            orderBy: "startTime",
+          },
+          (error, response) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(response);
+            }
           }
-          return resolve(token);
-        });
+        );
       })
         .then((token) => {
           // Respond with OAuth token 
