6ec294113fd29bfa7cc96188d05937f9522e28c6
karenlyyys <99241654+karenlyyys@users.noreply.github.com>
2022-08-22 10:45:33 -0700

    msg

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -13,8 +13,17 @@ test('render number input', () => {
     expect(NumberOfEventsWrapper.find('.rendernumber')).toHaveLength(1);
 });
 
-test('render default number of events as 32', () => {
+test('render display 32 by default', () => {
     expect(NumberOfEventsWrapper.state('renderNumber')).toBe(32);
+});
+
+
+//CHANGE NUMBER OF EVENTS 
+test('user change number of events', () => {
+ NumberOfEventsWrapper.find('.renderNumber').simulate(
+      'change', {target: { value: 5 }}
+  );
+  expect(NumberOfEventsWrapper.state('renderNumber')).toEqual(5);
 });
 
 test('render change state when input changes', () => {
