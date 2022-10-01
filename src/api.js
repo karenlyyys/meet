890cca7a3a896c6276cd8a351d2f38e46dc5a72f
karenlyyys <99241654+karenlyyys@users.noreply.github.com>
2022-09-30 21:51:37 -0700

    implemented local storage

--- src/api.js
+++ src/api.js
@@ -89,8 +89,13 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const data = localStorage.getItem('lastEvents');
+    NProgress.done();
+    return data ? JSON.parse(data).events : [];
+  }
 
-  const token = await getAccessToken();
+const token = await getAccessToken();
   
   if (token) {
     removeQuery();
